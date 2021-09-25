092c8215938341bb6cfb6d5bcbd6cffe780e20c3
Shaikh Sabir <89734545+Sabirshaikh44@users.noreply.github.com>
2021-09-25 14:48:07 +0530

    adding files
    added all the filess

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("hello");
+// respnsivness to navbar:
+let hamburger = document.getElementById('hamburger');
+let links = document.getElementById('links');
+hamburger.addEventListener('click',function(){
+    if(links.style.display != 'block'){
+        links.style.display = 'block';
+    }
+    else{
+        links.style.display = 'none';
+    }
+})
+
+
+
+    
